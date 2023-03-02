eb08ec8d1220173d1e83908c92221692658f55a9
MrN KMP <80095731+babynut-history@users.noreply.github.com>
2023-03-02 22:48:35 +0700

    Project Update
    Add link to my socials.

--- script.js
+++ script.js
@@ -20,5 +20,15 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     })
+
+    $('.first li').click(function(){
+      $(this).toggleClass("shadow-1").siblings();
+      $(this).toggleClass("fill-color").siblings();
+    });
+    $('.second li').click(function(){
+      $(this).toggleClass("shadow-2").siblings();
+      $(this).toggleClass("fill-color").siblings();
+    });
+
 });
 
